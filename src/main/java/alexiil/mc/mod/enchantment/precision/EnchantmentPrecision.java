4948aabdf8f0227396aa1dc4cc6ad88bb24bd9fc
AlexIIL <alexj9@me.com>
2017-06-17 00:53:20 +0100

    Make it work on 1.11.2

---
+++ src/main/java/alexiil/mc/mod/enchantment/precision/EnchantmentPrecision.java
@@ -0,0 +1,46 @@
+package alexiil.mc.mod.enchantment.precision;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnumEnchantmentType;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
+
+public class EnchantmentPrecision extends Enchantment {
+
+    private static final EntityEquipmentSlot[] SLOTS = { //
+        EntityEquipmentSlot.MAINHAND, EntityEquipmentSlot.OFFHAND //
+    };
+
+    public static final EnchantmentPrecision INSTANCE = new EnchantmentPrecision();
+
+    private EnchantmentPrecision() {
+        super(Rarity.COMMON, EnumEnchantmentType.DIGGER, SLOTS);
+        setRegistryName(ToolModPrecise.MODID, "precision");
+        setName("precision");
+    }
+
+    /** Returns the minimal value of enchantability needed on the enchantment level passed. */
+    @Override
+    public int getMinEnchantability(int enchantmentLevel) {
+        return 1 + 10 * (enchantmentLevel - 1);
+    }
+
+    /** Returns the maximum value of enchantability nedded on the enchantment level passed. */
+    @Override
+    public int getMaxEnchantability(int enchantmentLevel) {
+        return super.getMinEnchantability(enchantmentLevel) + 50;
+    }
+
+    /** Returns the maximum level that the enchantment can have. */
+    @Override
+    public int getMaxLevel() {
+        return 1;
+    }
+
+    /** Determines if this enchantment can be applied to a specific ItemStack. */
+    @Override
+    public boolean canApply(ItemStack stack) {
+        return stack.getItem() == Items.SHEARS ? true : super.canApply(stack);
+    }
+}
---
+++ src/main/java/alexiil/mc/mod/enchantment/precision/ToolModPrecise.java
@@ -0,0 +1,47 @@
+package alexiil.mc.mod.enchantment.precision;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import net.minecraftforge.common.ForgeHooks;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+@EventBusSubscriber
+@Mod(modid = ToolModPrecise.MODID, version = "${version}")
+public class ToolModPrecise {
+
+    public static final String MODID = "tool_enchantment_precise";
+    @Mod.Instance
+    public static ToolModPrecise INSTANCE;
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event) {
+        GameRegistry.register(EnchantmentPrecision.INSTANCE);
+    }
+
+    @SubscribeEvent
+    public static void getBreakSpeed(PlayerEvent.BreakSpeed event) {
+        EntityPlayer player = event.getEntityPlayer();
+        ItemStack stack = player.getHeldItemMainhand();
+        int level = EnchantmentHelper.getEnchantmentLevel(EnchantmentPrecision.INSTANCE, stack);
+        if (level > 0 && event.getOriginalSpeed() > 1.0F) {
+            IBlockState state = event.getState();
+            World world = player.world;
+            BlockPos pos = event.getPos();
+
+            float hardness = state.getBlockHardness(world, pos);
+            if (hardness > 0 && ForgeHooks.canHarvestBlock(state.getBlock(), player, world, pos)) {
+                event.setNewSpeed(29.9F * hardness);
+            }
+        }
+    }
+}
